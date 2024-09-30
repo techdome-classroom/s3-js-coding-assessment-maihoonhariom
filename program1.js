eeb5a650d4dfa87e84f697a5df27cafe2880cff3
maihoonhariom <rajputhariom835@gmail.com>
2024-09-30 15:53:55 +0530

    Done

--- program1.js
+++ program1.js
@@ -3,7 +3,23 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack =[];
+    const machingBrack= {
+        ')':'(',
+        '}':'{',
+        '[':']'
+    }
+    for(let char of s){
+        if(char in machingBrack){
+            if(stack.length === 0 || stack.pop() !== machingBrack[char]){
+               return false;
+            }
+        }
+        else{
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
--- program2.js
+++ program2.js
@@ -3,7 +3,26 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const map = new Map();
+    map.set('I',1);
+    map.set('V',5);
+    map.set('X',10);
+    map.set('L',50);
+    map.set('C',100);
+    map.set('D',500);
+    map.set('M',1000);
+
+    let num = map.get(s.charAt(s.length-1));
+
+    for(let i = s.length -1; i>0; i--){
+        if(map.get(s.charAt(i)) > map.get(s.charAt(i-1)) ){
+            num-= map.get(s.charAt(i-1));
+        }
+        else{
+            num+= map.get(s.charAt(i-1));
+        }
+    }
+    return num;
 };
 
 
